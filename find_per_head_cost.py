416f0720baa5e1c83cc217379e7b779c1cfc7ff7
vijayaramanp <44201057+vijayaramanp@users.noreply.github.com>
2019-03-02 11:29:38 +0530

    Create find_per_head_cost.py

---
+++ find_per_head_cost.py
@@ -0,0 +1,17 @@
+#PF-Assgn-3
+#This verification is based on string match.
+
+mileage=12
+amount_per_litre=72
+distance_one_way=102
+per_head_cost=0
+divisible_by_five=False
+
+#Start writing your code from here
+#Populate the variables: per_head_cost and divisible_by_five
+
+
+
+#Do not modify the below print statements for verification to work
+print(per_head_cost)
+print(divisible_by_five)
